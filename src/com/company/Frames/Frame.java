9967aa859c96008fc8389647946a79041978bcca
Dmytro Klishch <klishch.dmytro@gmail.com>
2022-09-16 10:45:32 +0300

    Added Frame and MainFrame

---
+++ src/com/company/Frames/Frame.java
@@ -0,0 +1,7 @@
+package com.company.Frames;
+
+import javax.swing.*;
+
+public abstract class Frame extends JFrame {
+    public abstract void setUp();
+}
---
+++ src/com/company/Frames/MainFrame.java
@@ -0,0 +1,47 @@
+package com.company.Frames;
+
+
+import com.company.Frames.DataStuctures.DataStructuresFrame;
+import com.company.Frames.GraphAgorithms.GraphAlgorithmsFrame;
+import com.company.Frames.Listeners.MenuMoveActiveListener;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static com.company.Frames.Utils.ButtonUtils.createButton;
+
+public class MainFrame extends Frame {
+    private static final MainFrame instance = new MainFrame();
+
+    private MainFrame() {
+    }
+
+    public static MainFrame getInstance() {
+        return instance;
+    }
+
+    private JButton createGraphAlgorithmsButton() {
+        return createButton("Graph Algorithms", new MenuMoveActiveListener(this, GraphAlgorithmsFrame.getInstance()));
+    }
+
+    private JButton createDataStructuresButton() {
+        return createButton("Data Structures", new MenuMoveActiveListener(this, DataStructuresFrame.getInstance()));
+    }
+
+    private void addComponents() {
+        add(createGraphAlgorithmsButton());
+        add(createDataStructuresButton());
+    }
+
+    @Override
+    public void setUp() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(new FlowLayout());
+
+        addComponents();
+        setSize(100 * 4, 50 * 4);
+        setResizable(false);
+        setVisible(true);
+    }
+}
