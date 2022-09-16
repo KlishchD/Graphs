291bda0afed2059eec29d6da7f29f3a6e06cf036
Dmytro Klishch <klishch.dmytro@gmail.com>
2022-09-16 10:28:58 +0300

    Added Grid Graph class

---
+++ src/com/company/Graphs/Implementations/GridGraph.java
@@ -0,0 +1,29 @@
+package com.company.Graphs.Implementations;
+
+import com.company.Graphs.Algorithms.GridGraphAlgorithms.GridPoint;
+import com.company.Graphs.Errors.EdgeAlreadyExistsException;
+import com.company.Graphs.Errors.NoSuchVertexException;
+import com.company.Graphs.Errors.VertexAlreadyExistsException;
+
+public class GridGraph extends UnDirectedGraph<GridPoint, Integer> {
+    public GridGraph(int rows, int cols) {
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+                addVertexAndEdges(row, col);
+            }
+        }
+    }
+
+    private void addVertexAndEdges(int row, int col) {
+        try {
+            addVertex(new GridPoint(row, col), 0);
+            if (row > 0) {
+                addEdge(new GridPoint(row, col), new GridPoint(row - 1, col));
+            }
+            if (col > 0) {
+                addEdge(new GridPoint(row, col), new GridPoint(row, col - 1));
+            }
+        } catch (VertexAlreadyExistsException | EdgeAlreadyExistsException | NoSuchVertexException ignored) {
+        }
+    }
+}
