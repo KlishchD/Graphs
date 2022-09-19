fbaa41879c39b904aba133ffdeaa4d1042ce225c
Dmytro Klishch <klishch.dmytro@gmail.com>
2022-09-19 11:14:10 +0300

    Added tests

--- src/test/AbstractGraphTest.java
+++ src/test/AbstractGraphTest.java
@@ -1,2 +1,152 @@
-package PACKAGE_NAME;public class AbstractGraphTest {
+import com.company.Graphs.AbstractGraph;
+import com.company.Graphs.Errors.EdgeAlreadyExistsException;
+import com.company.Graphs.Errors.NoSuchVertexException;
+import com.company.Graphs.Errors.VertexAlreadyExistsException;
+import com.company.Graphs.Implementations.UnDirectedGraph;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test abstract graph functionality using UnDirectedGraph
+ */
+public class AbstractGraphTest {
+
+    @Test
+    public void addVertex_addsVertexes() throws VertexAlreadyExistsException, NoSuchVertexException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i < 100; ++i) {
+            graph.addVertex(i, i);
+        }
+        boolean isCorrect = true;
+        for (int i = 0; i < 100; ++i) {
+            isCorrect = isCorrect && graph.containsVertex(i);
+        }
+        assertTrue(isCorrect);
+    }
+
+    @Test
+    public void addVertexTwice_trowsException() throws VertexAlreadyExistsException, NoSuchVertexException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        Exception exception = assertThrows(VertexAlreadyExistsException.class, () -> {
+            graph.addVertex(0, 0);
+            graph.addVertex(0, 1);
+        });
+        assertEquals("Vertex 0 already exists", exception.getMessage());
+    }
+
+    @Test
+    public void removeVertex_removesVertex() throws VertexAlreadyExistsException, NoSuchVertexException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        graph.addVertex(0, 0);
+        graph.removeVertex(0);
+        assertFalse(graph.containsVertex(0));
+    }
+
+    @Test
+    public void removeVertexTwice_throwsException() throws VertexAlreadyExistsException, NoSuchVertexException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        graph.addVertex(0, 0);
+        graph.removeVertex(0);
+        Exception exception = assertThrows(NoSuchVertexException.class, () -> graph.removeVertex(0));
+        assertEquals("There is no such vertex 0", exception.getMessage());
+
+    }
+
+    @Test
+    public void removeNonExistingVertex_throwsException() {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        Exception exception = assertThrows(NoSuchVertexException.class, () -> graph.removeVertex(0));
+        assertEquals("There is no such vertex 0", exception.getMessage());
+    }
+
+    @Test
+    public void getVertexValue_returnsValue() throws NoSuchVertexException, VertexAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i < 100; ++i) {
+            graph.addVertex(i, i);
+        }
+
+        boolean isCorrect = true;
+        for (int i = 0; i < 100; ++i) {
+            isCorrect = isCorrect && (i == graph.getVertexValue(i));
+        }
+        assertTrue(isCorrect);
+    }
+
+    @Test
+    public void getNonExistingVertexValue_throwsException() {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        Exception exception = assertThrows(NoSuchVertexException.class, () -> graph.getVertexValue(0));
+        assertEquals("There is no such vertex 0", exception.getMessage());
+    }
+
+    @Test
+    public void getAllVertexesIds_returnsAllVertexesIds() throws VertexAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i < 100; ++i) {
+            graph.addVertex(i, i);
+        }
+        List<Integer> expected = new ArrayList<>();
+        for (int i = 0; i < 100; ++i) {
+            expected.add(i);
+        }
+
+        assertEquals(expected, graph.getAllVertexesIds());
+    }
+
+    @Test
+    public void isGraphConnectedForConnectedGraph_returnsTrue() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i < 10; ++i) {
+            graph.addVertex(i, i);
+        }
+        for (int i = 1; i < 10; ++i) {
+            graph.addEdge(i - 1, i);
+        }
+        assertTrue(graph.isGraphConnected());
+    }
+
+    @Test
+    public void isGraphConnectedForNonConnectedGraph_returnsTrue() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i < 10; ++i) {
+            graph.addVertex(i, i);
+        }
+        for (int i = 1; i < 9; ++i) {
+            graph.addEdge(i - 1, i);
+        }
+        assertFalse(graph.isGraphConnected());
+    }
+
+    @Test
+    public void calculateShortestDistanceBetweenVertexesForConnectedVertexes_returnsDistances() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i < 10; ++i) {
+            graph.addVertex(i, i);
+        }
+        for (int i = 1; i < 9; ++i) {
+            graph.addEdge(i - 1, i);
+        }
+        int result = graph.calculateShortestDistanceBetweenVertexes(0, 2);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void calculateShortestDistanceBetweenVertexesForNonConnectedVertexes_returnsDistances() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i < 10; ++i) {
+            graph.addVertex(i, i);
+        }
+        for (int i = 1; i < 9; ++i) {
+            graph.addEdge(i - 1, i);
+        }
+        int result = graph.calculateShortestDistanceBetweenVertexes(0, 9);
+        assertEquals(Integer.MAX_VALUE, result);
+    }
+
 }
--- src/test/UnDirectedGraphTest.java
+++ src/test/UnDirectedGraphTest.java
@@ -1,2 +1,177 @@
-package PACKAGE_NAME;public class UnDirectedGraphTest {
+import com.company.Graphs.AbstractGraph;
+import com.company.Graphs.Errors.EdgeAlreadyExistsException;
+import com.company.Graphs.Errors.NoSuchEdgeException;
+import com.company.Graphs.Errors.NoSuchVertexException;
+import com.company.Graphs.Errors.VertexAlreadyExistsException;
+import com.company.Graphs.Implementations.UnDirectedGraph;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UnDirectedGraphTest {
+
+    @Test
+    public void addEdge_addsEdge() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        graph.addVertex(0, 0);
+        graph.addVertex(1, 0);
+        graph.addEdge(0, 1);
+        assertTrue(graph.containsEdge(0, 1) && graph.containsEdge(1, 0));
+    }
+
+    @Test
+    public void addEdgeWithNonExistingFirstVertex_throwsException() throws VertexAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        graph.addVertex(1, 0);
+        Exception exception = assertThrows(NoSuchVertexException.class, () -> graph.addEdge(0, 1));
+        assertEquals("There is no such vertex 0", exception.getMessage());
+    }
+
+    @Test
+    public void addEdgeWithNonExistingSecondVertex_throwsException() throws VertexAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        graph.addVertex(0, 0);
+        Exception exception = assertThrows(NoSuchVertexException.class, () -> graph.addEdge(0, 1));
+        assertEquals("There is no such vertex 1", exception.getMessage());
+    }
+
+    @Test
+    public void addAlreadyExistingEdge_throwsException() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        graph.addVertex(0, 0);
+        graph.addVertex(1, 0);
+        graph.addEdge(0, 1);
+        Exception exception = assertThrows(EdgeAlreadyExistsException.class, () -> graph.addEdge(0, 1));
+        assertEquals("Edge between 0 and 1 already exists", exception.getMessage());
+    }
+
+    @Test
+    public void removeEdge_removesEdge() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException, NoSuchEdgeException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        graph.addVertex(0, 0);
+        graph.addVertex(1, 0);
+        graph.addEdge(0, 1);
+        graph.removeEdge(0, 1);
+        assertTrue(!graph.containsEdge(0, 1) && !graph.containsEdge(1, 0));
+    }
+
+    @Test
+    public void removeEdgeForNonExistingFirstVertex_throwsException() throws VertexAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        graph.addVertex(1, 0);
+        Exception exception = assertThrows(NoSuchVertexException.class, () -> graph.removeEdge(0, 1));
+        assertEquals("There is no such vertex 0", exception.getMessage());
+    }
+
+    @Test
+    public void removeEdgeForNonExistingSecondVertex_throwsException() throws VertexAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        graph.addVertex(0, 0);
+        Exception exception = assertThrows(NoSuchVertexException.class, () -> graph.removeEdge(0, 1));
+        assertEquals("There is no such vertex 1", exception.getMessage());
+    }
+
+    @Test
+    public void removeEdgeForNonExistingEdge_throwsException() throws VertexAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        graph.addVertex(0, 0);
+        graph.addVertex(1, 0);
+        Exception exception = assertThrows(NoSuchEdgeException.class, () -> graph.removeEdge(0, 1));
+        assertEquals("There is no such edge between 0 and 1", exception.getMessage());
+    }
+
+    @Test
+    public void removeEdgeForAlreadyRemovedEdge_throwsException() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException, NoSuchEdgeException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        graph.addVertex(0, 0);
+        graph.addVertex(1, 0);
+        graph.addEdge(0, 1);
+        graph.removeEdge(0, 1);
+        Exception exception = assertThrows(NoSuchEdgeException.class, () -> graph.removeEdge(0, 1));
+        assertEquals("There is no such edge between 0 and 1", exception.getMessage());
+    }
+
+
+    @Test
+    public void getAllDirectlyConnectedVertexesForConnectedVertex_returnsAllDirectlyConnectedVertexes() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i < 10; ++i) {
+            graph.addVertex(i, i);
+        }
+        for (int i = 1; i < 10; ++i) {
+            graph.addEdge(0, i);
+        }
+
+        List<Integer> expected = new ArrayList<>();
+        for (int i = 1; i < 10; ++i) {
+            expected.add(i);
+        }
+
+        assertEquals(expected, graph.getAllDirectlyConnectedVertexes(0));
+    }
+
+    @Test
+    public void getAllDirectlyConnectedVertexesForNonConnected_returnsAllDirectlyConnectedVertexes() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException {
+        AbstractGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i <= 10; ++i) {
+            graph.addVertex(i, i);
+        }
+        for (int i = 1; i < 10; ++i) {
+            graph.addEdge(0, i);
+        }
+
+        assertTrue(graph.getAllDirectlyConnectedVertexes(10).isEmpty());
+    }
+
+    @Test
+    public void removeAllEdgesForVertexForNonExistingVertex_removesEdges() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException {
+        UnDirectedGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i < 10; ++i) {
+            graph.addVertex(i, i);
+        }
+        for (int i = 1; i < 10; ++i) {
+            graph.addEdge(0, i);
+        }
+
+        Exception exception = assertThrows(NoSuchVertexException.class, () -> graph.removeAllEdgesForVertex(11));
+        assertEquals("There is no such vertex 11", exception.getMessage());
+    }
+
+    @Test
+    public void removeAllEdgesForVertexForRemovedVertex_removesEdges() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException {
+        UnDirectedGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i < 10; ++i) {
+            graph.addVertex(i, i);
+        }
+        for (int i = 1; i < 10; ++i) {
+            graph.addEdge(0, i);
+        }
+        graph.removeVertex(0);
+        Exception exception = assertThrows(NoSuchVertexException.class, () -> graph.removeAllEdgesForVertex(0));
+        assertEquals("There is no such vertex 0", exception.getMessage());
+    }
+
+    @Test
+    public void removeAllEdgesForVertexForConnectedVertex_removesEdges() throws VertexAlreadyExistsException, NoSuchVertexException, EdgeAlreadyExistsException {
+        UnDirectedGraph<Integer, Integer> graph = new UnDirectedGraph<>();
+        for (int i = 0; i < 10; ++i) {
+            graph.addVertex(i, i);
+        }
+        for (int i = 1; i < 10; ++i) {
+            graph.addEdge(0, i);
+        }
+
+        graph.removeAllEdgesForVertex(0);
+
+        boolean isCorrect = true;
+        for (int i = 0; i < 10; ++i) {
+            isCorrect = isCorrect && graph.getAllDirectlyConnectedVertexes(i).isEmpty();
+        }
+        assertTrue(isCorrect);
+    }
+
+
 }
