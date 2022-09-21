16aa721c43565a3064fa8d0989486383c91083f7
Dmytro Klishch <klishch.dmytro@gmail.com>
2022-09-21 16:16:44 +0300

    Addded new windows to set up list

--- src/java/com/company/Frames/GUI.java
+++ src/java/com/company/Frames/GUI.java
@@ -1,8 +1,12 @@
 package com.company.Frames;
 
-import com.company.Frames.GraphAgorithms.GraphAlgorithmsFrame;
-import com.company.Frames.GraphAgorithms.GridGraphAlgorithmFrame;
-import com.company.Frames.GraphAgorithms.GridResizingFrame;
+import com.company.Frames.GraphAgorithms.ArbitraryGraphAlgorithms.MangeEdgeArbitraryGraphRenderingFrame;
+import com.company.Frames.GraphAgorithms.ArbitraryGraphAlgorithms.ManageVertexArbitraryGraphRenderingFrame;
+import com.company.Frames.GraphAgorithms.ArbitraryGraphAlgorithms.ArbitraryGraphAlgorithmRenderFrame;
+import com.company.Frames.GraphAgorithms.ArbitraryGraphAlgorithms.ArbitraryGraphAlgorithmsSelectFrame;
+import com.company.Frames.GraphAgorithms.GridGrpahAlgorithms.GridGraphAlgorithmsSelectFrame;
+import com.company.Frames.GraphAgorithms.GridGrpahAlgorithms.GridGraphAlgorithmRenderingFrame;
+import com.company.Frames.GraphAgorithms.GridGrpahAlgorithms.GridResizingFrame;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,10 +15,14 @@ public class GUI {
     public static void start() {
         List<Frame> frames = Arrays.asList(
                 MainFrame.getInstance(),
-                GraphAlgorithmsFrame.getInstance(),
-                GridGraphAlgorithmFrame.getInstance(),
-                GridGraphAlgorithmFrame.getInstance(),
-                GridResizingFrame.getInstance()
+                GridGraphAlgorithmsSelectFrame.getInstance(),
+                GridGraphAlgorithmRenderingFrame.getInstance(),
+                GridGraphAlgorithmRenderingFrame.getInstance(),
+                GridResizingFrame.getInstance(),
+                ArbitraryGraphAlgorithmRenderFrame.getInstance(),
+                ArbitraryGraphAlgorithmsSelectFrame.getInstance(),
+                ManageVertexArbitraryGraphRenderingFrame.getInstance(),
+                MangeEdgeArbitraryGraphRenderingFrame.getInstance()
         );
         frames.forEach(Frame::setUp);
     }
