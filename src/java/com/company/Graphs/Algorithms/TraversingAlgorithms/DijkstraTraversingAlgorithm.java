9da45eec04c17a6330a27866ee52471450016507
Dmytro Klishch <klishch.dmytro@gmail.com>
2022-09-23 10:47:03 +0300

    Refactored Dijkstra algorithm

--- src/java/com/company/Graphs/Algorithms/TraversingAlgorithms/DijkstraTraversingAlgorithm.java
+++ src/java/com/company/Graphs/Algorithms/TraversingAlgorithms/DijkstraTraversingAlgorithm.java
@@ -1,5 +1,6 @@
 package com.company.Graphs.Algorithms.TraversingAlgorithms;
 
+import com.company.Graphs.Algorithms.Tuple;
 import com.company.Graphs.Errors.NoSuchVertexException;
 import com.company.Graphs.GraphInterface;
 
@@ -7,10 +8,10 @@ import java.util.*;
 
 public class DijkstraTraversingAlgorithm<T> implements GraphTraversingAlgorithm<T, Integer> {
 
-    private PriorityQueue<Tuple> getPriorityQueue(GraphInterface<T, Integer> graph) {
-        PriorityQueue<Tuple> order = new PriorityQueue<>();
+    private PriorityQueue<Tuple<T, Integer>> getPriorityQueue(GraphInterface<T, Integer> graph) {
+        PriorityQueue<Tuple<T, Integer>> order = new PriorityQueue<>();
         for (T point : graph.getPointsOfType(GraphInterface.PointType.SOURCE)) {
-            order.add(new Tuple(0, null, point));
+            order.add(new Tuple<>(0, null, point));
         }
         return order;
     }
@@ -26,23 +27,30 @@ public class DijkstraTraversingAlgorithm<T> implements GraphTraversingAlgorithm<
         return distances;
     }
 
+    private void addVertexes(GraphInterface<T, Integer> graph, PriorityQueue<Tuple<T, Integer>> order, Map<T, Integer> distances, T point) throws NoSuchVertexException {
+        for (T to : graph.getAllDirectlyConnectedVertexes(point)) {
+            if (distances.get(point) + graph.getEdgeValue(point, to) <= distances.get(to)) {
+                distances.put(to, distances.get(point) + graph.getEdgeValue(point, to));
+                order.add(new Tuple<>(distances.get(to), point, to));
+            }
+        }
+    }
+
+    private void makeNextDijkstraIteration(GraphInterface<T, Integer> graph, PriorityQueue<Tuple<T, Integer>> order, Map<T, Integer> distances, Set<T> visited, Map<T, T> result) throws NoSuchVertexException {
+        Tuple<T, Integer> tuple = order.poll();
+        if (visited.contains(tuple.getTo())) return;
+        result.put(tuple.getTo(), tuple.getFrom());
+        addVertexes(graph, order, distances, tuple.getTo());
+        visited.add(tuple.getTo());
+    }
+
     private Map<T, T> dijkstra(GraphInterface<T, Integer> graph) throws NoSuchVertexException {
-        PriorityQueue<Tuple> order = getPriorityQueue(graph);
+        PriorityQueue<Tuple<T, Integer>> order = getPriorityQueue(graph);
         Map<T, Integer> distances = getDistances(graph);
         Set<T> visited = new LinkedHashSet<>();
         Map<T, T> result = new LinkedHashMap<>();
         while (!order.isEmpty()) {
-            Tuple tuple = order.poll();
-            if (visited.contains(tuple.getTo())) continue;
-            T current = tuple.getTo();
-            result.put(tuple.getTo(), tuple.getFrom());
-            for (T to : graph.getAllDirectlyConnectedVertexes(current)) {
-                if (distances.get(current) + graph.getEdgeValue(current, to) <= distances.get(to)) {
-                    distances.put(to, distances.get(current) + graph.getEdgeValue(current, to));
-                    order.add(new Tuple(distances.get(to), current, to));
-                }
-            }
-            visited.add(current);
+            makeNextDijkstraIteration(graph, order, distances, visited, result);
         }
         return result;
     }
@@ -54,35 +62,6 @@ public class DijkstraTraversingAlgorithm<T> implements GraphTraversingAlgorithm<
         } catch (NoSuchVertexException e) {
             e.printStackTrace();
             return null;
-        }
-    }
-
-    private class Tuple implements Comparable<Tuple> {
-        private final Integer value;
-        private final T from;
-        private final T to;
-
-        public Tuple(Integer value, T from, T to) {
-            this.value = value;
-            this.from = from;
-            this.to = to;
-        }
-
-        @Override
-        public int compareTo(Tuple o) {
-            return Integer.compare(value, o.value);
-        }
-
-        public Integer getValue() {
-            return value;
-        }
-
-        public T getFrom() {
-            return from;
-        }
-
-        public T getTo() {
-            return to;
         }
     }
 }
